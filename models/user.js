9123a8978f2ccc396211527409417df97d45da7d
Benjamin <benjamin.lewin@yahoo.com>
2023-03-18 13:33:04 -0400

    created user associations

--- models/user.js
+++ models/user.js
@@ -10,7 +10,17 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Content, {
+        foreignKey: 'user_id',
+        as: 'contents',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
+      User.hasMany(models.Comment, {
+        foreignKey: 'user_id',
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      })
     }
   }
   User.init({
