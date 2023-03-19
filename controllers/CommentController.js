3665459f7cdb7e47ab42efc87283fa0bf6b0f52e
Toni <windtronic89@gmail.com>
2023-03-19 15:02:58 -0600

    Please Look over :)

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,66 +1,86 @@
-const { User, Content, Comment } = require('../models')
+const { Comment, User, Content } = require('../models')
+
 
 const GetAllComments = async (req, res) => {
     try {
-      const comments = await Content.findAll()
+      const comments = await Comment.findAll()
       res.send(comments)
     } catch (error) {
       throw error
     }
   }
-  
+
 const GetComment = async (req, res) => {
-    try {
-      const comment = await Comment.findByPk(req.params.comment_id)
-      res.send(comment)
-    } catch (error) {
-      throw error
-    }
+try{
+    let userId = req.params.user_id;
+    let comments = await Comment.findAll({
+        where: {
+            user_id : userId
+        }
+    })
+    res.send(comments)
+} 
+    catch (error) {
+    throw error
+}
+}
+
+const GetRecentComment = async (req, res) => {
+  try {
+    const recent = await Comment.findAll({ order: [['created_at', 'DESC']] })
+    res.send(recent)
+  } catch (error) {
+    throw error
   }
-  
+}
+
 const CreateComment = async (req, res) => {
-    try {
-      let userId = parseInt(req.params.user_id)
-      let commentBody = {
-        userId,
-        ...req.body
-      }
-      let comment = await Comment.create(commentBody)
-      res.send(comment)
-    } catch (error) {
-      throw error
-    }
+  try {
+    let contentId = parseInt(req.params.content_id);
+    let commentBody = {
+      contentId,
+      ...req.body,
+    };
+    let comment = await Comment.create(commentBody);
+    res.send(comment);
+  } catch (error) {
+    throw error;
   }
-  
-const UpdateComment = async (req, res) => {
-    try {
-      let commentId = parseInt(req.params.comment_id)
-      let updatedComment = await Comment.update(req.body, {
-        where : { id: commentId},
-        returning: true
-      })
-      res.send(updatedComment)
-    } catch (error) {
-      throw error
-    }
+};
+
+const UpdateComment = async(req,res) => {
+  try{
+    let commentId = parseInt(req.params.comment_id)
+    let updatedComment = await Comment.update(req.body, {
+      where: {id: commentId},
+      returning: true
+    })
+    res.send(updatedComment)
+  } catch (error) {
+    throw error
   }
-  
-const DeleteComment = async (req, res) => {
-    try {
-      let commentId = parseInt(req.params.comment_id)
-      await Comment.destroy({
-        where: { id: commentId }
-      })
-      res.send(`deleted comment id ${commentId}`)
-    } catch (error) {
-      throw error
-    }
+}
+const DeleteComment = async(req,res) => {
+  try{
+    let commentId = parseInt(req.params.comment_id)
+    await Comment.destroy({where: {id: commentId}})
+    res.send(`deleted comment id ${commentId}`)
+  } catch (error) {
+    throw error
   }
+}
+
+
+
+
+
 
 module.exports = {
+  
     GetAllComments,
-    GetComment,
+    GetRecentComment,
     CreateComment,
     UpdateComment,
-    DeleteComment
+    DeleteComment,
+    GetComment
 }
--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -1,16 +1,12 @@
 const Router = require('express').Router()
 const controller = require('../controllers/CommentController')
 
-
-
 Router.get('/', controller.GetAllComments)
+Router.get('/recents', controller.GetRecentComment)
 Router.get('/view/:comment_id', controller.GetComment)
-Router.post('/:user_id', controller.CreateComment)
+Router.post('/:content_id', controller.CreateComment)
 Router.put('/:comment_id', controller.UpdateComment)
 Router.delete('/:comment_id', controller.DeleteComment)
-
-
-
 
 
 
