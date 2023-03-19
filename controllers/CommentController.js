45a968e6d9e3e85715c566f6334a76c7d3ad0979
Toni <windtronic89@gmail.com>
2023-03-19 14:00:55 -0600

    still working

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,25 +1,66 @@
-const { Comment, User, Content } = require('../models')
-
+const { User, Content, Comment } = require('../models')
 
 const GetAllComments = async (req, res) => {
     try {
-      const comments = await Comment.findAll()
+      const comments = await Content.findAll()
       res.send(comments)
     } catch (error) {
       throw error
     }
   }
-
-
-
-
-
-
-
+  
+const GetComment = async (req, res) => {
+    try {
+      const comment = await Comment.findByPk(req.params.comment_id)
+      res.send(comment)
+    } catch (error) {
+      throw error
+    }
+  }
+  
+const CreateComment = async (req, res) => {
+    try {
+      let userId = parseInt(req.params.user_id)
+      let commentBody = {
+        userId,
+        ...req.body
+      }
+      let comment = await Comment.create(commentBody)
+      res.send(comment)
+    } catch (error) {
+      throw error
+    }
+  }
+  
+const UpdateComment = async (req, res) => {
+    try {
+      let commentId = parseInt(req.params.comment_id)
+      let updatedComment = await Comment.update(req.body, {
+        where : { id: commentId},
+        returning: true
+      })
+      res.send(updatedComment)
+    } catch (error) {
+      throw error
+    }
+  }
+  
+const DeleteComment = async (req, res) => {
+    try {
+      let commentId = parseInt(req.params.comment_id)
+      await Comment.destroy({
+        where: { id: commentId }
+      })
+      res.send(`deleted comment id ${commentId}`)
+    } catch (error) {
+      throw error
+    }
+  }
 
 module.exports = {
-    
-    GetAllComments
-   
-    
+    GetAllComments,
+    GetComment,
+    CreateComment,
+    UpdateComment,
+    DeleteComment
 }
--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -2,7 +2,12 @@ const Router = require('express').Router()
 const controller = require('../controllers/CommentController')
 
 
+
 Router.get('/', controller.GetAllComments)
+Router.get('/view/:comment_id', controller.GetComment)
+Router.post('/:user_id', controller.CreateComment)
+Router.put('/:comment_id', controller.UpdateComment)
+Router.delete('/:comment_id', controller.DeleteComment)
 
 
 
